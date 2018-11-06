6cfe83c4a91d6e9cd1425522e06874b331f92b0a
Mep Pep <Mitchell_Pepe@yahoo.com>
2018-11-06 01:44:46 -0500

    ...

---
+++ TCM.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod
+{
+    public class ExampleMod : Mod
+    {
+        public ExampleMod()
+        {
+
+        }
+
+        public override void Load()
+        {
+
+            On.Terraria.GameContent.UI.Elements.UIGenProgressBar.DrawSelf += UIGenProgressBarDrawSelf;
+
+            if (!Main.dedServ)
+            {
+
+            }
+        }
+
+        private void UIGenProgressBarDrawSelf(On.Terraria.GameContent.UI.Elements.UIGenProgressBar.orig_DrawSelf orig, Terraria.GameContent.UI.Elements.UIGenProgressBar self, SpriteBatch spriteBatch)
+        {
+            
+        }
+
+        public override void Unload()
+        {
+            if (!Main.dedServ)
+            {
+
+            }
+        }
+    }
+}
